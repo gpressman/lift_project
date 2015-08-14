c2a582a3df0699c14be51526276ee46f7e4a3f05
Gabe Pressman <pressman4747@gmail.com>
2015-08-13 23:29:49 -0400

    scaffold stats

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :stats
+
   devise_for :users
   devise_for :views
   # The priority is based upon order of creation: first created -> highest priority.
