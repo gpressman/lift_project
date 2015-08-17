acb89c7b947fe9ea90098661a15655d76bb1b856
Gabe Pressman <pressman4747@gmail.com>
2015-08-16 20:27:00 -0400

    fixed resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
  
   def after_sign_in_path_for(resource)
-    new_weight_path
+    new_user_weight_path
   end
   protect_from_forgery with: :exception
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-
-  resources :weights
+  devise_for :users, :path => 'accounts'
+    resources :users do
+      resources :weights
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
