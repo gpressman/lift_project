d983cd78291cf292d62e869b523e21ed6cdb0288
Gabe Pressman <pressman4747@gmail.com>
2015-11-25 18:19:19 -0500

    date formatted better

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,19 +1,22 @@
 class AttemptsController < ApplicationController
   def index
+    @user = User.find(params[:id])
   	@exercise = Exercise.find(params[:id])
   	@attempts = @exercise.attempts.all
   end
 
   def new
+    @user = User.find(params[:id])
   	@exercise = Exercise.find(params[:id])
   	@attempt = @exercise.attempts.new	
   end
 
   def create
+    @user = User.find(params[:id])
   	@exercise = Exercise.find(params[:id])
   	@attempt = @exercise.attempts.new(attempt_params)
   	@attempt.user_id = @exercise.user_id
-  	  if attempt.save
+  	  if @attempt.save
   	  	redirect_to exercise_attempts(@exercise.id)
   	  else
   	  	render 'new_exercise_attempt'
