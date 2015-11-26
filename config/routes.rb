66156bb3f8290b98ce46d63d4734790220a331cc
Gabe Pressman <pressman4747@gmail.com>
2015-11-26 13:00:59 -0500

    refactor resource routes so everything extends from 'user'

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   devise_for :users, :path => 'accounts'
     resources :users, only: [:show, :index] do 
       resources :weights, only: [:create, :show, :new, :index]
-    end
-    resources :exercises, only: [:create, :show, :new, :index, :destroy] do
+      resources :exercises, only: [:create, :show, :new, :index, :destroy]
       resources :attempts, only: [:create, :new, :index]
     end
+     
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
