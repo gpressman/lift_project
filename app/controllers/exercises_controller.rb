74d088d0491c1b7c5a02fe44a51e26359e03364c
Gabe Pressman <pressman4747@gmail.com>
2016-01-20 14:21:50 -0500

    makes chart for exercises over time

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -13,7 +13,53 @@ class ExercisesController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @exercise = @user.exercises.find(params[:id])
-    @attempts = @exercise.attempts.where(user_id: current_user.id)
+    @attempts = @exercise.attempts.where(user_id: current_user.id)   
+    attempts = @attempts.select(:score, :created_at)
+    scores_over_time = []
+    attempts.each do |attempt|
+      scores_over_time.push({
+        label: attempt.created_at.strftime("%m-%-d-%y"),
+        value: attempt.score,
+      })
+    end
+    @exercise_chart = Fusioncharts::Chart.new({
+    type: 'area2d',
+    renderAt: 'chart-container',
+    width: '400',
+    height: '300',
+    dataFormat: 'json',
+    dataSource: {
+        "chart": {
+            "caption": "#{@exercise.name} over time",
+                
+            "xAxisName": "Date",
+            "yAxisName": "Weight",
+            "paletteColors": "#0075c2",
+            "bgColor": "#ffffff",
+            "showBorder": "0",
+            "showCanvasBorder": "0",
+            "plotBorderAlpha": "10",
+            "usePlotGradientColor": "0",
+            "plotFillAlpha": "50",
+            "showXAxisLine": "1",
+            "axisLineAlpha": "25",
+            "divLineAlpha": "10",
+            "showValues": "1",
+            "showAlternateHGridColor": "0",
+            "captionFontSize": "14",
+            "subcaptionFontSize": "14",
+            "subcaptionFontBold": "0",
+            "toolTipColor": "#ffffff",
+            "toolTipBorderThickness": "0",
+            "toolTipBgColor": "#000000",
+            "toolTipBgAlpha": "80",
+            "toolTipBorderRadius": "2",
+            "toolTipPadding": "5"
+            },
+            data: scores_over_time
+        }
+    })
+  
   end
 
   # GET /exercises/new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,55 @@
 class UsersController < ApplicationController
   def show
-  	@user = User.find(params[:id])
+  	
+  @user = User.find(params[:id])  
+
+@weights = @user.weights
+weights = @weights.select(:weight, :created_at)
+weights_over_time = []
+
+
+weights.each do |weight|
+weights_over_time.push({
+    label: weight.created_at.strftime("%m-%-d-%y"),
+    value: weight.weight,
+})
+end
+@weights_chart = Fusioncharts::Chart.new({
+    type: 'area2d',
+    renderAt: 'chart-container',
+    width: '400',
+    height: '300',
+    dataFormat: 'json',
+    dataSource: {
+        "chart": {
+            "caption": "Weight over time",
+                
+            "xAxisName": "Date",
+            "yAxisName": "Weight",
+            "paletteColors": "#0075c2",
+            "bgColor": "#ffffff",
+            "showBorder": "0",
+            "showCanvasBorder": "0",
+            "plotBorderAlpha": "10",
+            "usePlotGradientColor": "0",
+            "plotFillAlpha": "50",
+            "showXAxisLine": "1",
+            "axisLineAlpha": "25",
+            "divLineAlpha": "10",
+            "showValues": "1",
+            "showAlternateHGridColor": "0",
+            "captionFontSize": "14",
+            "subcaptionFontSize": "14",
+            "subcaptionFontBold": "0",
+            "toolTipColor": "#ffffff",
+            "toolTipBorderThickness": "0",
+            "toolTipBgColor": "#000000",
+            "toolTipBgAlpha": "80",
+            "toolTipBorderRadius": "2",
+            "toolTipPadding": "5"
+            },
+            data: weights_over_time
+        }
+    })
   end
 end
