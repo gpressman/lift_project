4aae665901465c0c58ae58e47e55ce53e16b3057
Gabe Pressman <pressman4747@gmail.com>
2016-02-07 00:54:02 -0500

    refactored all the json out of examples controller

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -15,39 +15,7 @@ weights_over_time.push({
 })
 end
 @chart = Fusioncharts::Chart.new({
-    type: 'area2d',
-    renderAt: 'chart-container',
-    width: '400',
-    height: '300',
-    dataFormat: 'json',
-    dataSource: {
-        "chart": {
-            "caption": "Weight over time",
-                
-            "xAxisName": "Date",
-            "yAxisName": "Weight",
-            "paletteColors": "#0075c2",
-            "bgColor": "#ffffff",
-            "showBorder": "0",
-            "showCanvasBorder": "0",
-            "plotBorderAlpha": "10",
-            "usePlotGradientColor": "0",
-            "plotFillAlpha": "50",
-            "showXAxisLine": "1",
-            "axisLineAlpha": "25",
-            "divLineAlpha": "10",
-            "showValues": "1",
-            "showAlternateHGridColor": "0",
-            "captionFontSize": "14",
-            "subcaptionFontSize": "14",
-            "subcaptionFontBold": "0",
-            "toolTipColor": "#ffffff",
-            "toolTipBorderThickness": "0",
-            "toolTipBgColor": "#000000",
-            "toolTipBgAlpha": "80",
-            "toolTipBorderRadius": "2",
-            "toolTipPadding": "5"
-            },
+        
             data: weights_over_time
         }
     })
--- app/models/example.rb
+++ app/models/example.rb
@@ -1,3 +1,39 @@
 class Example
 	
+  class Fusioncharts::Chart
+  	"data":{
+  	    type: 'area2d',
+  	    renderAt: 'chart-container',
+  	    width: '400',
+  	    height: '300',
+  	    dataFormat: 'json',
+  	    dataSource: {
+  	        "chart": {
+  	            "caption": "Weight over time",
+  	                
+  	            "xAxisName": "Date",
+  	            "yAxisName": "Weight",
+  	            "paletteColors": "#0075c2",
+  	            "bgColor": "#ffffff",
+  	            "showBorder": "0",
+  	            "showCanvasBorder": "0",
+  	            "plotBorderAlpha": "10",
+  	            "usePlotGradientColor": "0",
+  	            "plotFillAlpha": "50",
+  	            "showXAxisLine": "1",
+  	            "axisLineAlpha": "25",
+  	            "divLineAlpha": "10",
+  	            "showValues": "1",
+  	            "showAlternateHGridColor": "0",
+  	            "captionFontSize": "14",
+  	            "subcaptionFontSize": "14",
+  	            "subcaptionFontBold": "0",
+  	            "toolTipColor": "#ffffff",
+  	            "toolTipBorderThickness": "0",
+  	            "toolTipBgColor": "#000000",
+  	            "toolTipBgAlpha": "80",
+  	            "toolTipBorderRadius": "2",
+  	            "toolTipPadding": "5"
+  	            }}
+  	  end
 end
