a4caabf2791e737dd059704cc63ee51412af7872
Gabe Pressman <pressman4747@gmail.com>
2016-03-22 21:12:06 -0400

    changed login redirect to users#show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   # For APIs, you may want to use :null_session instead.
  
   def after_sign_in_path_for(resource)
-    new_user_weight_path(current_user.id)
+    user_path(current_user.id)
   end
   protect_from_forgery with: :exception
 
